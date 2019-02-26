3aa8ec6341519f8b89f29be7a9c7bba761595432
Ava Robinson <avamrobinson@gmail.com>
2019-02-26 16:21:14 -0600

    first push

---
+++ LogIn.js
@@ -0,0 +1,7 @@
+LogIn(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	if (email == "avamrobinson@gmail.com"){
+		window.location.href = "HomePage.html";
+	}
+}
