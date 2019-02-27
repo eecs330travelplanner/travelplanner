85d3074aed9c5c7797dbe36d6a7ebdbc47d7e8d2
Ava Robinson <avamrobinson@gmail.com>
2019-02-26 18:18:48 -0600

    second commit

--- LogIn.js
+++ LogIn.js
@@ -1,7 +1,11 @@
-LogIn(){
+function LogIn(){
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
 	if (email == "avamrobinson@gmail.com"){
+		document.getElementByName("log-in-form").setAttribute("action", "HomePage.html");
 		window.location.href = "HomePage.html";
+	}
+	else{
+		document.getElementById("invalid-input").innerHTML = "incorrect email or password"
 	}
 }
