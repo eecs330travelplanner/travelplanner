318c4e5e915626fb92920b406f1a2babe3b97c4b
Ava Robinson <avamrobinson@gmail.com>
2019-02-27 01:06:22 -0600

    log in complete

--- LogIn.js
+++ LogIn.js
@@ -9,3 +9,35 @@ function LogIn(){
 		document.getElementById("invalid-input").innerHTML = "Incorrect email or password."
 	}
 }
+
+// function instagram(){
+// 	document.getElementsByClassName("create_account_box")[0].style.display = "none";
+// 	document.getElementsByClassName("create_new_account")[0].style.display = "none";
+// 	document.getElementsByClassName("continue-button")[0].style.display = "none";
+
+// 	document.getElementsByClassName("instagram-login-box")[0].style.display = "block";
+// }
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+function btnfunction() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function spanfunction() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function spanfunction2() {
+  document.getElementsByClassName("insta-complete")[0].style.display = "inline";
+  setTimeout(function(){document.getElementById("myModal").style.display = "none";}, 1000)
+}
