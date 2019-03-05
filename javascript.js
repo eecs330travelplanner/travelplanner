8943f491a8338837e58aac8cf952257a1341f8c8
Abby Pratt <abbypratt@dhcp-10-105-64-255.wireless.northwestern.private>
2019-03-04 22:05:57 -0600

    filter

--- javascript.js
+++ javascript.js
@@ -25,3 +25,49 @@ function buildBoard(wrap) {
 function suggestions(){
 	document.getElementsByClassName("activities")[0].style.display = "inline";
 }
+
+function filterSelection(ele,c) {
+	temp = ele.parentNode.getElementsByClassName("activeFilter")[0];
+	temp.classList.remove("activeFilter")
+	temp.classList.add("filterButton")
+	ele.classList.add("activeFilter")
+	ele.classList.remove("filterButton")
+
+	var x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c == "all") c = "";
+	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (i = 0; i < x.length; i++) {
+	    w3RemoveClass(x[i], "show");
+	    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+	}
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+
+
+
+
